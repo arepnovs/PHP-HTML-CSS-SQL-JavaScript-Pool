e72bc561a09f460988ace88e5659560ae20a0b55
Anton Repnovsky <repnovant@gmail.com>
2017-06-14 12:09:52 +0300

    Update ssap2.php

--- Day01/ex09/ssap2.php
+++ Day01/ex09/ssap2.php
@@ -1,91 +1,59 @@
 #!/usr/bin/php
 <?php
 
-function if_space($arg)
+function get_ascii($char)
 {
+	$ascii = ord($char);
+	if ($ascii == 0)
+		return $ascii;
+	if (($ascii < 48) || ($ascii >= 91 && $ascii <= 96) || ($ascii >= 123))
+		$ascii = $ascii + 1000;
+	else if (is_numeric($char))
+		$ascii = $ascii + 100;
+	else if (ctype_upper($char))
+		$ascii = $ascii + 32;
+	return $ascii;
+}
+
+function ascii_compare($str1, $str2)
+{
+	if ($str1 == $str2)
+		return 0;
+	$s1 = str_split($str1, 1);
+	$s2 = str_split($str2, 1);
+	$s1_len = strlen($str1);
+	$s2_len = strlen($str2);
 	$i = 0;
-	while ($arg[$i])
+	while ($i < $s1_len && $i < $s2_len)
 	{
-		if ($arg[$i] == " ")
-			return 1;
+		$asc1 = get_ascii($s1[$i]);
+		$asc2 = get_ascii($s2[$i]);
+		if ($asc1 != $asc2)
+			return ($asc1 < $asc2) ? -1 : 1;
 		$i++;
 	}
-	return 0;
+	if ($i == $s1_len && $i == $s2_len)
+		return (0);
+	if ($i < $s2_len)
+		return (-1);
+	return 1;
 }
 
-function ft_split($str)
+if ($argc < 2)
+    exit (1);
+unset($argv[0]);
+$elms = array();
+foreach ($argv as $el)
 {
-	$arr = explode(" ", $str);
-	if (!empty($arr)|| strcmp("0", $el) == 0)
-	{ 
-		foreach ($arr as $el)
-		{
-			if (!empty($el) || strcmp("0", $el) == 0)
-				$res[] = $el;
-		}
-		return $res;
-	}
+	$exploded = explode(" ", $el);
+    $filtered = array_filter($exploded);
+    $splitted = array_slice($filtered, 0);
+    $elms = array_merge($elms, $splitted);
 }
-
-$i = 0;
-$f = 0;
-if ($argc > 1)
+usort($elms, "ascii_compare");
+foreach ($elms as $el)
 {
-	if (!empty($arr)|| strcmp("0", $el) == 0)
-	{
-		foreach ($argv as $el)
-		{
-			if ($i != 0 && if_space($el) == 0)
-				$prii[] = $el;
-			else if ($i != 0)
-			{
-				$prii2[]= ft_split($el);
-				$f++;
-			}
-			$i++;
-		}
-		$tot = count($prii2);
-		$i = 1;
-		if ($f != 0)
-		{
-			$final = array_merge($prii, $prii2[0]);
-			while ($i < $tot)
-			{
-				$final = array_merge($final, $prii2[$i]);
-				$i++;
-			}
-		}
-		else
-			$final = array_merge($prii);
-		foreach ($final as $el) 
-		{
-			if (is_numeric($el))
-				$nums[] = $el;
-			else if (($el[0] >= "A" && $el[0] <= "Z") || ($el[0] >= "a" && $el[0] <= "z"
-				|| ($el[0] >= "0" && $el[0] <= "9")))
-				$alph[] = $el;
-			else
-				$sign[] = $el;
-		}
-		if (!empty($alph))
-		{
-			sort($alph, SORT_NATURAL | SORT_FLAG_CASE);
-			foreach ($alph as $el)
-				echo"$el\n";
-		}
-		if (!empty($nums) || strcmp("0", $el) == 0)
-		{
-			sort($nums, SORT_STRING);
-			foreach ($nums as $el)
-				echo"$el\n";
-		}
-		if (!empty($sign))
-		{
-			sort($sign);
-			foreach ($sign as $el)
-				echo"$el\n";
-		}
-	}
+    echo $el."\n";
 }
 
 ?>
